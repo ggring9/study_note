9fb05deb99eeff876bc29537d753dd50eccb8079
ParkJiHwan <ggring9@gmail.com>
2021-06-27 15:39:06 +0900

    init git

---
+++ refactoring_note/src/refactoring_note/FindInt.java
@@ -0,0 +1,15 @@
+package refactoring_note;
+public class FindInt {
+
+	public static boolean find(int[] data, int target) {
+	
+		boolean flag = false;
+		
+		for (int i = 0; i < data.length && !flag; i++) {
+			if (data[i] == target) {
+				flag = true;
+			}
+		}
+		return flag;
+	}
+}
